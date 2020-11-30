7ac08ae2df96ce8d345d177b837f2bc143743776
Trevis42 <sirtrevis42@gmail.com>
2020-11-30 13:43:04 -0600

    fixed broken test

--- src/main/java/com/github/perschola/TableUtilities.java
+++ src/main/java/com/github/perschola/TableUtilities.java
@@ -2,14 +2,22 @@ package com.github.perschola;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int column = 1; column <= tableSize; column++) {
+            for (int row = 1; row <= tableSize; row++) {
+                int product = column * row;
+                result.append(String.format("%3d |", product));
+            }
+            result.append("\n");
+        }
+        return result.toString();
     }
 }
