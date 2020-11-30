fbd804a903c0400e2276fe159315732c87c6de59
Trevis42 <sirtrevis42@gmail.com>
2020-11-30 13:23:33 -0600

    updated

--- src/main/java/com/github/perschola/NumberUtilities.java
+++ src/main/java/com/github/perschola/NumberUtilities.java
@@ -3,34 +3,38 @@ package com.github.perschola;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        return getRange(start%2 == 0 ? start : start+1, stop, 2);
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        return getRange(start%2 != 0 ? start : start+1, stop, 2);
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        return getExponentiations(start,stop,step,2);
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        return getRange(0, stop, 1);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return getRange(start, stop, 1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        return getExponentiations(start, stop, step, 1);
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = start; i < stop; i+=step) {
+            result.append(Double.valueOf(Math.pow(i,exponent)).intValue());
+        }
+        return result.toString();
     }
 }
--- src/main/java/com/github/perschola/TriangleUtilities.java
+++ src/main/java/com/github/perschola/TriangleUtilities.java
@@ -3,18 +3,22 @@ package com.github.perschola;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder result = new StringBuilder();
+        for (int i = 1; i < numberOfRows; i++) {
+            result.append(getRow(i)).append("\n");
+        }
+        return result.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        return new String(new char[numberOfStars]).replaceAll("\u0000", "*");
     }
 
     public static String getSmallTriangle() {
-        return null;
+        return getTriangle(5);
     }
 
     public static String getLargeTriangle() {
-        return null;
+        return getTriangle(10);
     }
 }
